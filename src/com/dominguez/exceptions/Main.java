a811d78dd5c43c21120ecb4af5604296fc92bf3e
Chuck Charles <bmd2018@hotmail.com>
2018-02-27 13:29:41 -0500

    exercise 6

--- src/com/dominguez/exceptions/Main.java
+++ src/com/dominguez/exceptions/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         promptString("Enter in a string value.");
         promptInt("Enter in an integer.");
+        promptDouble("Enter in a double");
     }
     public static String promptString(String message){
         System.out.println(message);
@@ -35,6 +36,21 @@ public class Main {
         catch(InputMismatchException e){
             input.next();
             System.out.println("You need to enter an integer");
+            return 0;
+        }
+    }
+    public static double promptDouble(String message){
+        System.out.println(message);
+        Scanner input = new Scanner(System.in);
+
+        try{
+            int number = input.nextInt();
+
+            return input.nextInt();
+        }
+        catch(InputMismatchException e){
+            input.next();
+            System.out.println("You need to enter a double");
             return 0;
         }
     }
