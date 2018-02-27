c15409cb5652e28e256ea7a0103fb8f117e6450e
Chuck Charles <bmd2018@hotmail.com>
2018-02-27 13:21:05 -0500

    exercise 6

---
+++ src/com/dominguez/exceptions/Main.java
@@ -0,0 +1,41 @@
+package com.dominguez.exceptions;
+import java.text.ParseException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        promptString("Enter in a string value.");
+        promptInt("Enter in an integer.");
+    }
+    public static String promptString(String message){
+        System.out.println(message);
+        Scanner input = new Scanner(System.in);
+
+        try{
+
+            String string = input.nextLine();
+            return string;
+        }
+        catch(NullPointerException e){
+            input.next();
+            System.out.println("You need to enter something");
+            return "Anything";
+        }
+
+    }
+    public static int  promptInt(String message){
+        System.out.println(message);
+        Scanner input = new Scanner(System.in);
+
+        try{
+            int number = input.nextInt();
+
+            return input.nextInt();
+        }
+        catch(InputMismatchException e){
+            input.next();
+            System.out.println("You need to enter an integer");
+            return 0;
+        }
+    }
+}
