0c43f2faf99eb9042ff8cd67b8d4fe39e1ae059a
Chuck Charles <bmd2018@hotmail.com>
2018-04-11 23:16:42 -0400

    project3

--- src/com/dominguez/project3/Main.java
+++ src/com/dominguez/project3/Main.java
@@ -10,6 +10,15 @@ class Task implements Comparable<Task> {
     private String description;
     private int priority;
 
+    static class NameComparator implements Comparator{
+        public int compare(Object o1,Object o2){
+            Task s1=(Task)o1;
+            Task s2=(Task)o2;
+
+            return s1.name.compareTo(s2.name);
+        }
+    }
+
     Task(String name, String description, int priority) {
         setName(name);
         setDescription(description);
@@ -137,64 +146,59 @@ class TaskCollection
     }
 }
 
-class TaskView
-{
+class TaskView {
     private TaskCollection taskCollection;
     private Scanner scanner = new Scanner(System.in);
 
-    TaskView(TaskCollection collection)
-    {
+    TaskView(TaskCollection collection) {
         this.taskCollection = collection;
     }
 
-    private boolean isInteger(String string)
-    {
-        try
-        {
+    private boolean isInteger(String string) {
+        try {
             Integer.parseInt(string);
-        }
-        catch (NumberFormatException|NullPointerException e)
-        {
+        } catch (NumberFormatException | NullPointerException e) {
             return false;
         }
         return true;
     }
 
-    private String prompt(String message)
-    {
+    private String prompt(String message) {
         System.out.println(message);
         return this.scanner.nextLine();
     }
 
-    private int promptInt(String message)
-    {
+    private int promptInt(String message) {
         String response = prompt(message);
 
         boolean isInt = isInteger(response);
-        while (!isInt)
-        {
+        while (!isInt) {
             response = prompt(message);
             isInt = isInteger(response);
         }
         return Integer.parseInt(response);
     }
 
-    private void add()
-    {
+    private void add() {
         String name = prompt("What is the new task's name?");
         String description = prompt("What is the new task's description?");
         int priority = promptInt("What is the new task's priority?");
         this.taskCollection.addTask(name, description, priority);
     }
 
-    private void remove()
-    {
+    private void remove() {
         int index = promptInt("What is the index of the task you want to remove?");
         this.taskCollection.removeTask(index);
     }
 
-    private void update()
-    {
+    private void update(List<Task> tasks) {
+        for (int i = 0; i < tasks.size(); i++) {
+            Task task = (Task) tasks.get(i);
+            System.out.println("Task index: " + i + ", Name: " + task
+                    .getName() + ", Description: " + task
+                    .getDescription() + ", Priority: " + task
+                    .getPriority());
+        }
         int index = promptInt("What is the index of the task you want to update.");
         String newName = prompt("Enter the new name of the task.");
         String newDescription = prompt("Enter the new Description of the task");
@@ -202,17 +206,68 @@ class TaskView
         this.taskCollection.updateTask(index, newName, newDescription, newPriority);
     }
 
-    private void displayTasks(List<Task> tasks)
-    {
-        for (int i = 0; i < tasks.size(); i++)
-        {
-            Task task = (Task)tasks.get(i);
-            System.out.println("Task index: " + i + ", Name: " + task
-                    .getName() + ", Description: " + task
-                    .getDescription() + ", Priority: " + task
-                    .getPriority());
+    private void displayTasks(List<Task> tasks) {
+        List priority1 = new ArrayList();
+        List priority2 = new ArrayList();
+        List priority3 = new ArrayList();
+        List priority4 = new ArrayList();
+        List priority5 = new ArrayList();
+        for (int i = 0; i < tasks.size(); i++) {
+            Task task = (Task) tasks.get(i);
+            int priority = task.getPriority();
+            String name = task.getName();
+            String description = task.getDescription();
+            int index = i;
+            if (priority == 1) {
+                priority1.add(task);
+                this.taskCollection.updateTask(index, name, description, priority);
+                Collections.sort(priority1, new Task.NameComparator());
+            }
+            if (priority == 2) {
+                priority2.add(task);
+                this.taskCollection.updateTask(index, name, description, priority);
+                Collections.sort(priority2, new Task.NameComparator());
+            }
+            if (priority == 3) {
+                priority3.add(task);
+                this.taskCollection.updateTask(index, name, description, priority);
+                Collections.sort(priority3, new Task.NameComparator());
+            }
+            if (priority == 4) {
+                priority4.add(task);
+                this.taskCollection.updateTask(index, name, description, priority);
+                Collections.sort(priority4, new Task.NameComparator());
+            }
+            if (priority == 5) {
+                priority5.add(task);
+                this.taskCollection.updateTask(index, name, description, priority);
+                Collections.sort(priority5, new Task.NameComparator());
+            }
+        }
+        for (int i = 0; i < priority1.size(); i++) {
+            Task task = (Task)priority1.get(i);
+            System.out.println("Name: " + task.getName()  + ", Description: " + task.getDescription() + ", Priority: 1" );
+        }
+        for (int i = 0; i < priority2.size(); i++) {
+            Task task = (Task)priority2.get(i);
+            System.out.println("Name: " + task.getName()  + ", Description: " + task.getDescription() + ", Priority: 2" );
+        }
+        for (int i = 0; i < priority3.size(); i++) {
+            Task task = (Task)priority3.get(i);
+            System.out.println("Name: " + task.getName()  + ", Description: " + task.getDescription() + ", Priority: 3" );
+        }
+        for (int i = 0; i < priority4.size(); i++) {
+            Task task = (Task)priority4.get(i);
+            System.out.println("Name: " + task.getName()  + ", Description: " + task.getDescription() + ", Priority: 4" );
+        }
+        for (int i = 0; i < priority5.size(); i++) {
+            Task task = (Task)priority5.get(i);
+            System.out.println("Name: " + task.getName()  + ", Description: " + task.getDescription() + ", Priority: 5" );
         }
     }
+
+
+
 
 
 
@@ -256,7 +311,7 @@ class TaskView
                     remove();
                     break;
                 case 3:
-                    update();
+                    update(this.taskCollection.getTasks());
                     break;
                 case 4:
                     list();
@@ -278,14 +333,7 @@ public class Main {
         TaskCollection collection = new TaskCollection();
         TaskView view = new TaskView(collection);
         view.run();
-     //   Task display = new Task();
-       // Task.displayTasks();
 
-      //  Collections.sort(TaskCollection.tasks);
-
-      //  for (Task contact : tasks) {
-         //   contact.display();
         }
     }
-    //}
 
